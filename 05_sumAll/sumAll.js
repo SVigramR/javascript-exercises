1da1fa560852026891582fbc0b835e9f7e574b58
SVigramR <sabareeshvigram654@gmail.com>
2022-07-11 00:19:54 +0530

    Completed sunAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let totalSum = 0;
+    let firstNum = firstInt;
+    let secondNum = secondInt;
 
+    if(firstNum < 0 || secondNum < 0){
+        return "ERROR";
+    } else if(typeof firstNum === 'string' || typeof secondNum === 'string'){
+        return "ERROR";
+    } else if(Array.isArray(firstNum) || Array.isArray(secondNum)){
+        return "ERROR";
+    }
+
+    if(firstNum > secondNum){
+        for( let i = 0;i <= firstNum;i++){
+            totalSum += i
+        }
+    } else {
+        for( let i = 0;i <= secondNum;i++){
+            totalSum += i
+        }
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
