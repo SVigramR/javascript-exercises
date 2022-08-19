a2caca8256bdbe3c700c4bec42864ce65d026595
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-19 22:23:08 +0530

    Completed calculator exercise
    Also installed npm package to run jest in windows vs code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrInt) {
+	let totalSum = 0;
+  for (let index = 0; index < arrInt.length; index++) {
+    totalSum += arrInt[index];    
+  }
+  return parseInt(totalSum);
 };
 
-const multiply = function() {
-
+const multiply = function(arrMul) {
+  let totalMulti = 1;
+  for (let index = 0; index < arrMul.length; index++) {
+    totalMulti *= arrMul[index];    
+  }
+  return parseInt(totalMulti);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(fac) {
+  let totalFac = 1;
+  let i = 1;
+  while (i <= fac) {
+    totalFac *= i;
+    i++;
+  }
+  return totalFac;
 };
 
 // Do not edit below this line
