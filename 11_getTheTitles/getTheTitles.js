fe9ff08b609484e649946265ee6641901d84b730
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-20 22:23:03 +0530

    Completed getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let array = [];
+    let title;
+    for (let index = 0; index < arr.length; index++) {
+        title = arr[index].title;
+        array.push(title)
+    }
+    return array;
 };
 
 // Do not edit below this line
