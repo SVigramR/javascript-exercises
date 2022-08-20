b5861571359d9b163d58e944f219f617bb46439e
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-20 21:30:54 +0530

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let number = parseInt(num),
+        firstNum = 0,
+        secNum = 1,
+        sum = 1;
 
+    for (let index = 1; index < number; index++) {
+        sum = firstNum + secNum;
+        firstNum = secNum;
+        secNum = sum;
+    }
+
+    if (number < 0) {
+        return "OOPS";
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
