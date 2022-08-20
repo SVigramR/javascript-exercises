e9ece609b523ecef96d1f4c9f5b3131493354140
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-20 16:23:51 +0530

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let string = str.toLowerCase().replace(/[^a-zA-Z]/g, '');
+    let revStr = string.split('').reverse().toString().replace(/,/g, '');;
 
+    if (string === revStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
