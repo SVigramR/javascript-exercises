e303e3372e046d4b5dece8cea1b4c3a5a3d08fdd
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-20 22:57:10 +0530

    Completed findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+	return array.reduce((oldest, currentPerson) => {
+	  const oldestAge = age(oldest.yearOfBirth, oldest.yearOfDeath)
+	  const currentAge = age(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+	  return oldestAge < currentAge ? currentPerson : oldest
+	})
+};
 
+const age = function(birth, death) {
+	if (!death) {
+	  death = new Date().getFullYear();	
+	}
+	return death - birth;
 };
 
 // Do not edit below this line
